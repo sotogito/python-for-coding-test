f8e943a6678d6f1f01d7af4fc2629b75848f238d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-25 16:16:34 +0900

    refactor (3-3 숫자 카드 게임)

--- sotogito/greedy/A_number_card_game.py
+++ sotogito/greedy/A_number_card_game.py
@@ -3,7 +3,7 @@
 m : 가로
 n : 세로
 """
-
+"""
 m,n = map(int, input().split())
 cards = [list(map(int, input().split())) for _ in range(m)]
 
@@ -15,4 +15,15 @@ for card in cards:
         result = smallest
 
 print(result)
+"""
 
+# 입력을 받으면서 바로 해결하기
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    card = list(map(int, input().split()))
+    result = max(min(card), result)  # NOTE 바로 변수에 큰 값 비교해서 넣기
+
+print(result)

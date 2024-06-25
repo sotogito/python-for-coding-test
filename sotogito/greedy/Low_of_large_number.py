254113141cf711a334da6d9430473d499933af49
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-25 15:30:24 +0900

    refactor (3-2 큰 수의 법칙)

--- sotogito/greedy/Low_of_large_number.py
+++ sotogito/greedy/Low_of_large_number.py
@@ -3,7 +3,7 @@
 m : 인덱스 총 길이
 k : 인덱스 최대 연속
 """
-
+"""
 n, m, k = map(int, input().split())
 numbers = list(map(int, input().split()))
 
@@ -16,5 +16,19 @@ while len(result) != m:
     result.append(numbers[1])
 
 print(sum(result))
+"""
 
+n, m, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort(reverse=True)
 
+result = 0
+
+count = m // (k + 1) # 0000 0000
+additionalCount = m - (count * (k + 1)) # 0000 0000 0
+
+result += count * (k * numbers[0])
+result += count * numbers[1]
+result += additionalCount * numbers[0]
+
+print(result)

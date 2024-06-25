ce67a8173718d047f2106dc02c51531c13311ce8
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-25 16:16:07 +0900

    solve (3-4 1이 될 때까지)

---
+++ sotogito/greedy/Until_One.py
@@ -0,0 +1,26 @@
+"""
+1이 될떄까지
+n : 1이 돼야하는 수
+k : n에서 나누는 수
+
+1. n에서 1을 뺀다
+2. n에서 k로 나누다.
+"""
+
+
+def until_one(n, k):
+    if n % k == 0:
+        return n // k
+    return n - 1
+
+
+n, k = map(int, input().split())
+
+count = 0
+
+while True:
+    n = until_one(n, k)
+    count += 1
+    if n == 1:
+        print(count)
+        break

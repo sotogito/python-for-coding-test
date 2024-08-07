c6884471e18c72950d202a9852a4c00121ff844e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-07 17:39:49 +0900

    solve (게임 개발)

--- sotogito/implementation/game_development.py
+++ sotogito/implementation/game_development.py
@@ -1,81 +1,136 @@
 """
-- 왼쪽으로 회전
-- 왼쪽에 가보지 않은 칸 : 왼쪽 회전 - 이동
-- 왼쪽에 가본 칸 : 왼쪽 회전만
+1. 현재 위치에서 현재 방향을 기준으로 왼쪽 방향(반시계 방향으로 90도 회전한 방향)부터 차례대로 갈 곳을 정한다.
+-> 시계방향으로 먼저 회전해야됨
+2. 캐릭터의 바로 왼쪽 방향에 아직 가보지 않은 칸이 존재한다면, 왼쪽 방향으로 횐전한 다음 왼쪽으로 한 칸을 전진한다. 왼쪽 방향에 가보지 않은 칸이 없다면, 왼쪽 방향으로 회전만 수행하고 1단계로 돌아간다.
+-> 회전한 방향으로 위치를 옮겨야됨
+-> 회전과, 회전한 방향으로 칸을 옮기는 것은 다른 동작임
+3. 만약 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 칸인 경우에는, 바라보는 방향을 유지한 채로 한 칸 뒤로 가고 1단계로 돌아간다. 단, 이때 뒤쪽 방향이 바다인 칸이라 뒤로 갈 수 없는 경우에는 움직임을 멈춘다
 
-- 4면이 모두 가본칸이거나 or 바다일 경우 -> 뒤로 한칸(바다이면 이동 못함)
 
-1. 왼쪽 확인,
+
 """
 
-direction = [3, 2, 1, 0]  #서, 남, 동, 북
-left_direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-front_direction = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-back_direction = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-
-already_been_to = []
-
-land = 0
-sea = 1
+map_h, map_v = map(int, input().split())
+x, y, dir = map(int, input().split())  # 가로 세로 방향
 game_map = []
-
-h, v = map(int, input().split())
-hor, ver, dir = map(int, input().split())  #가로 세로 방향
-for i in range(h):
+for i in range(map_h):
     one_line = list(map(int, input().split()))
     game_map.append(one_line)
 
-user_location = [hor, ver]
-user_direction = dir
-move = 1
-can_not_move_count_down = 4
+
+
+gone_land = []
+direction = [0, 3, 2, 1]  # 왼쪽으로 회전 순 -> 북 서 남 동
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+
+
+move_count = 1
+not_move_count = 4
+
+
+def is_move(nx, ny):
+    if 0 <= nx < len(game_map) and 0 <= ny < len(game_map[0]):
+        if (game_map[nx][ny] == 0) and ((nx, ny) not in gone_land):
+            return True
+    return False
+
+def is_game_continue(nx, ny):
+    if 0 <= nx < len(game_map) and 0 <= ny < len(game_map[0]):
+        if (game_map[nx][ny] == 0):
+            return True
+    return False
+
 
 while True:
-    already_been_to.append([user_location[0], user_location[1]])
-    left_direction_index = direction.index(user_direction)
-    #next_direction_index = (direction_index + 1) % len(direction)
-    #left_direction_index = direction.index(user_direction)
-    left_value = left_direction[left_direction_index]
+    dir = (dir - 1) % 4  # note 4방향을 순환코드
+    nx = x + dx[direction.index(dir)]
+    ny = y + dy[direction.index(dir)]
 
-    next_x = user_location[0] + left_value[0]
-    next_y = user_location[1] + left_value[1]
 
-    is_move = True
-    if (0 <= next_x < h and 0 <= next_y < v and
-            (game_map[next_x][next_y] == sea or [next_x, next_y] in already_been_to)):
-        is_move = False
+    if is_move(nx, ny):
+        gone_land.append((x, y))
+        x, y = nx, ny
+        move_count += 1
+        not_move_count = 4
 
-    if can_not_move_count_down == 0:
-        back_direction_index = direction.index(user_direction)
-        back_value = back_direction[back_direction_index]
-        user_location = [user_location[0] + back_value[0], user_location[1] + back_value[1]]
-        next_x = user_location[0] + back_value[0]
-        next_y = user_location[1] + back_value[1]
+    elif not is_move(nx, ny):
+        not_move_count -= 1
 
-        is_move = True
-        if (0 <= next_x < h and 0 <= next_y < v and
-                (game_map[next_x][next_y] == sea or [next_x, next_y] in already_been_to)):
-            is_move = False
 
-        if not is_move:
+    if not_move_count == 0:
+        back_index = (dir + 1) % 4
+        nx = x - dx[direction.index(back_index)] # 1,0 / 2.1 / 1.2 / 0.1 동 : +1 0
+        ny = y - dy[direction.index(back_index)]
+
+        if is_game_continue(nx, ny):
+            x, y = nx, ny
+            move_count += 1
+            not_move_count = 4
+            continue
+        else:
             break
 
-    if is_move:  #움직일 수 있으면
-        user_direction = direction[(direction.index(user_direction) + 1) % len(direction)]
-        front_direction_index = direction.index(user_direction)  #전진하 계산 Index 가져오고
-        front_value = front_direction[front_direction_index]
-        next_x = user_location[0] + front_value[0]
-        next_y = user_location[1] + front_value[1]
+
+print(move_count)
 
 
 
-        user_location = [user_location[0] + front_value[0], user_location[1] + front_value[1]]
-        can_not_move_count_down = 4
-        move += 1
+
+
+
+
+
+
+
+
+
+"""
+
+map_h, map_v = map(int, input().split())
+x, y, dir = map(int, input().split())  # 초기 위치와 방향
+game_map = []
+for i in range(map_h):
+    one_line = list(map(int, input().split()))
+    game_map.append(one_line)
+
+# 방향은 북, 동, 남, 서 순서로 설정
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+visited = [[False] * map_v for _ in range(map_h)]
+visited[x][y] = True
+
+move_count = 1
+turn_time = 0
+
+while True:
+    # 왼쪽으로 회전
+    dir = (dir - 1) % 4
+    nx = x + dx[dir]
+    ny = y + dy[dir]
+
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하고, 바다가 아닌 경우 이동
+    if not visited[nx][ny] and game_map[nx][ny] == 0:
+        x, y = nx, ny
+        visited[x][y] = True
+        move_count += 1
+        turn_time = 0
         continue
-    elif not is_move:
-        user_direction = direction[(direction.index(user_direction) + 1) % len(direction)]
-        can_not_move_count_down -= 1
-        continue
+    else:  # 이동할 수 없는 경우
+        turn_time += 1
 
-print(move)
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[dir]
+        ny = y - dy[dir]
+        # 뒤로 갈 수 있다면 이동
+        if game_map[nx][ny] == 0:
+            x, y = nx, ny
+        else:  # 뒤가 바다로 막혀있는 경우
+            break
+        turn_time = 0
+
+print(move_count)
+
+"""

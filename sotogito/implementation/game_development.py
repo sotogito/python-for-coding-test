f053579343d3dd0e20bee7edb4530a58e095c161
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-07 19:11:04 +0900

    correct answer (게임 개발)

--- sotogito/implementation/game_development.py
+++ sotogito/implementation/game_development.py
@@ -1,98 +1,59 @@
-"""
-1. 현재 위치에서 현재 방향을 기준으로 왼쪽 방향(반시계 방향으로 90도 회전한 방향)부터 차례대로 갈 곳을 정한다.
--> 시계방향으로 먼저 회전해야됨
-2. 캐릭터의 바로 왼쪽 방향에 아직 가보지 않은 칸이 존재한다면, 왼쪽 방향으로 횐전한 다음 왼쪽으로 한 칸을 전진한다. 왼쪽 방향에 가보지 않은 칸이 없다면, 왼쪽 방향으로 회전만 수행하고 1단계로 돌아간다.
--> 회전한 방향으로 위치를 옮겨야됨
--> 회전과, 회전한 방향으로 칸을 옮기는 것은 다른 동작임
-3. 만약 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 칸인 경우에는, 바라보는 방향을 유지한 채로 한 칸 뒤로 가고 1단계로 돌아간다. 단, 이때 뒤쪽 방향이 바다인 칸이라 뒤로 갈 수 없는 경우에는 움직임을 멈춘다
+# N, M을 공백을 기준으로 구분하여 입력받기
+n, m = map(int, input().split())
 
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터의 X 좌표, Y 좌표, 방향을 입력받기
+x, y, direction = map(int, input().split())
+d[x][y] = 1 # 현재 좌표 방문 처리
 
-의문????????????????????????????????
-뒤쪽 방향이 바다인 경우???
-1 1 1 1
-1 0 0 1
-1 1 0 1
-1 1 1 1
-맵이 이거라면 0인 이동 가능한 땅은
-(1,1)
-(1,2)
-(2,2)
-인데
-(1,1) -> (1,2)로 이동할떄 방향은 동쪽을 보고 진입인데..
-그리고 (1,2) ->(2,2)이면 남쪽으로 보고 진입할텐데
-결국은 뒤로 이동할떄가 되면 각각 동쪽, 남쪽을 바라볼테고, 지나왔던 방향으로 돌아갈수 있냐를 따지는거 아닌가? 그럼 결국은 뒤쪽이 방향인 경우가 있나?
+# 전체 맵 정보를 입력받기
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
 
-그럼 결국 이동을 멈추는 경우는 뒤쪽 방향이 바다칸인 경우라 뒤로 이동할수 없는 경우가 아니라,
-4면에 바다 and 이미 가본 땅일때 멈추는 거 같다. = 바다만 아
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-뒤쪽 방향이 바다칸인 경우라 뒤로 이동할수 없는 경우는 모든 면이 다 바다인 경우만 해당된다.
-"""
+# 왼쪽으로 회전
+def turn_left(): #note 회전 계산 로직을 아예 따로 구현, 배열의 순서에 의존 x
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
 
-
-
-
-
-map_h, map_v = map(int, input().split())
-x, y, dir = map(int, input().split())  # 가로 세로 방향
-game_map = []
-for i in range(map_h):
-    one_line = list(map(int, input().split()))
-    game_map.append(one_line)
-
-
-
-gone_land = []
-direction = [0, 3, 2, 1]  # 왼쪽으로 회전 순 -> 북 서 남 동
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
-
-move_count = 1
-not_move_count = 4
-
-
-def is_move(nx, ny): #왼쪽으로 이동의 기준 : map 범위 안 && 육지인지 && 이미 가본 육지가 아닌지
-    if 0 <= nx < len(game_map) and 0 <= ny < len(game_map[0]):
-        if (game_map[nx][ny] == 0) and ((nx, ny) not in gone_land):
-            return True
-    return False
-
-def is_game_continue(nx, ny):
-        if (game_map[nx][ny] == 0):
-            return True
-        return False
-
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
 while True:
-    dir = (dir - 1) % 4     # note  현재 위치에서 현재 방향을 기준으로 왼쪽 방향    # note 4방향을 순환코드
-    nx = x + dx[direction.index(dir)]   #note 방향 기준 왼쪽 위치를 구함 -> 차례대로 갈 곳을 정한다
-    ny = y + dy[direction.index(dir)]
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
 
-
-    if is_move(nx, ny):     #note 바로 왼쪽 방향에 아직 가보지 않은 칸이 존재한다면
-        gone_land.append((x, y))    # 이미 가본 방향 저장
-        x, y = nx, ny   # note 왼쪽으로 한 칸을 전진한다
-        move_count += 1     # 움직임 업데이트
-        not_move_count = 4  # 초기화
-
-    elif not is_move(nx, ny):   #note 왼쪽 방향에 가보지 않은 칸이 없다면,
-        not_move_count -= 1  # note 왼쪽 방향으로 회전만 수행하고 1단계로 돌아간다.
-
-
-    if not_move_count == 0:     # note 만약 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 칸인 경우에는
-        nx = x - dy[direction.index(dir)]
-        ny = y - dx[direction.index(dir)]
-
-
-        if game_map[nx][ny] == 0: #note 한 칸 뒤로 가고 1단계로 돌아간다.
-            x, y = nx, ny
-            not_move_count = 4
-            continue
-
-        break #note 이때 뒤쪽 방향이 바다인 칸이라 뒤로 갈 수 없는 경우에는 움직임을 멈춘다
-
-
-print(move_count)
-
-
-
-
-
+# 정답 출력
+print(count)

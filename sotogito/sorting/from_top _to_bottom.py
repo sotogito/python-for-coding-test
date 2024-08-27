7997d7ecd6b8d3485c9509b0a3e6ebdef8562ef1
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-27 14:40:12 +0900

    resolve (위에서 아래로) : 내림차순으로 다시 수정

--- sotogito/sorting/from_top _to_bottom.py
+++ sotogito/sorting/from_top _to_bottom.py
@@ -4,10 +4,13 @@ array = []
 for i in range(n):
     array.append(int(input()))
 
+
+# array.sort(reverse=True)
+
 for i in range(n):
     min_index = i
     for j in range(i+1, n):
-        if array[j] < array[min_index]:
+        if array[j] > array[min_index]:
             min_index = j
     array[i], array[min_index] = array[min_index], array[i]
 

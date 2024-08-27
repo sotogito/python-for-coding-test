76ea4f2880f81fd7c22bd6eeadedd875b9a0ad7f
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-27 14:38:34 +0900

    solve (위에서 아래로)

---
+++ sotogito/sorting/from_top _to_bottom.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if array[j] < array[min_index]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+print(*array)

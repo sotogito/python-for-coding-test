7049f72152baa6b8acde344b610b5585821a7269
sotogito <108535526+sotogito@users.noreply.github.com>
2024-09-03 18:06:01 +0900

    resolve (두 배열의 원소 교체) : A원소가 더 클 경우 교환되는 오류 해결

--- sotogito/sorting/replacement_of_two_array_elements.py
+++ sotogito/sorting/replacement_of_two_array_elements.py
@@ -3,8 +3,6 @@
 
 풀이 : A배열의 최소값과 B배열의 최대값을 교환한다.
 
-
-
 """
 
 n,k = map(int,input().split())
@@ -16,7 +14,12 @@ arrayA.sort()
 arrayB.sort(reverse=True)
 
 for i in range(k):
-    arrayA[i],arrayB[i] = arrayB[i],arrayA[i]
+    #arrayA[i],arrayB[i] = arrayB[i],arrayA[i] # note 이것만 하면 만약 A 원소가 클 경우도 변경해버린다.
+
+    if arrayA[i] < arrayB[i]:
+        arrayA[i],arrayB[i] = arrayB[i],arrayA[i]
+        continue
+
 
 result = sum(arrayA)
 print(result)
